6f7c67ad024618908601757e56b911336e478b70
Aleksey Stupnikov <stuq11@gmail.com>
2021-07-08 01:17:59 +0500

    Реализована универсальная машина состояний

---
+++ src/core/stateMachine.js
@@ -0,0 +1,56 @@
+class State {
+
+    constructor (stateMachine) {
+        this.stateMachine = stateMachine;
+    }
+
+    onActivate () {
+
+    }
+
+    onDeactivate () {
+
+    }
+
+    onEvent (event) {
+
+    }
+
+}
+
+class StateMachine {
+
+    constructor () {
+        this._possibleStates = [];
+        this._idCurrentState = -1;
+    }
+
+    addPossibleState (possibleStateClass) {
+        const id = this._possibleStates.length;
+        this._possibleStates.push(new possibleStateClass(this));
+        return id;
+    }
+
+    setCurrentState (idNewState) {
+        if (this._idCurrentState !== -1) {
+            this._possibleStates[this._idCurrentState].onDeactivate();
+        }
+        this._idCurrentState = idNewState;
+        this._possibleStates[this._idCurrentState].onActivate();
+    }
+
+    getCurrentStateId () {
+        return this._idCurrentState;
+    }
+
+    getCurrentState () {
+        return this._possibleStates[this._idCurrentState];
+    }
+
+    onEvent (event) {
+        if (this._idCurrentState !== -1) {
+            this._possibleStates[this._idCurrentState].onEvent(event);
+        }
+    }
+
+}
